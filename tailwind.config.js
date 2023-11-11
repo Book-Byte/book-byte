f2c165f890e92a197bdb57bec8ac51bc1c6db7cf
Abdullah-5603 <abdullah72308@gmail.com>
2023-11-11 17:00:29 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "node_modules/keep-react/**/*.{js,jsx,ts,tsx}"
   ],
+  presets: [require("keep-react/src/keep-preset.js")],
   theme: {
     extend: {
       backgroundImage: {
