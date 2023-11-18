20bac179721c03becc0f3ec31b6f4c91b0aff09f
Abu Abdullah <102359047+Abdullah-5603@users.noreply.github.com>
2023-11-18 06:12:29 +0600

    Update page.js

--- src/app/store/page.js
+++ src/app/store/page.js
@@ -11,7 +11,7 @@ const Store = async () => {
 
     // console.log(allBooks);
     return (
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-20 md:p-7 bg-white'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-12 md:mt-20 md:p-7 bg-white'>
             {
                 allBooks.map(book => (
                     <BookCard key={book._id} image={book.image} title={book.title} author={book.author} _id={book._id} sellingPrice={book.sellingPrice}/>
