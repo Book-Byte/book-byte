5bc843254f9dd58f8373bdae4cea72a1b862ca8e
Abu Abdullah <102359047+Abdullah-5603@users.noreply.github.com>
2023-11-18 06:07:20 +0600

    Update BookCard.js

--- src/app/store/BookCard.js
+++ src/app/store/BookCard.js
@@ -5,8 +5,8 @@ import Image from "next/image";
 const BookCard = ({ title, image, author, sellingPrice, _id }) => {
 
     return (
-        <div className="rounded-t-2xl rounded-b-xl bg-white shadow-2xl md:w-80 w-72">
-            <Image className="md:w-80 md:h-96 w-72 h-80 rounded-t-2xl" src={image} alt={title} width={200} height={50} />
+        <div className="rounded-t-2xl rounded-b-xl bg-white shadow-2xl md:w-80 w-[300px]">
+            <Image className="md:w-80 md:h-96 w-[300px] h-80 rounded-t-2xl" src={image} alt={title} width={200} height={50} />
             <div className="relative z-10 flex flex-col space-y-1 mt-1 p-2 h-[190px]">
                 <p className="font-semibold text-xl">{title}</p>
                 <p className="font-semibold">Author: <span className="font-normal">{author}</span></p>
